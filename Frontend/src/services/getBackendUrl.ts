fd96921d7c0c9e3b5fb23c1c5815eba79e2dd6e5
bwalid <bouwachmawalid1@gmail.com>
2025-06-20 15:26:44 +0100

    production ready3

--- Frontend/src/services/getBackendUrl.ts
+++ Frontend/src/services/getBackendUrl.ts
@@ -1,6 +1,6 @@
 export function getBackendUrl(gpu: number) {
-  if (gpu === 0) return "http://backend-gpu0-service:9898";
-  if (gpu === 1) return "http://backend-gpu1-service:9898";
+  if (gpu === 0 || gpu === 1) return "http://backend-gpu0-service:9898";
+  if (gpu === 2 || gpu === 3) return "http://backend-gpu1-service:9898";
   // fallback to load balancer or default
   return "http://backend-loadbalancer-service:9898";
-} 
+}
